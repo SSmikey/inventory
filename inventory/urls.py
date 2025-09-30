d6a458460a1ad0f9e6adfe1ce872b55ce55a0428
SSmikey <Seangnon09@gmail.com>
2025-09-30 13:11:32 +0700

    add view home

--- inventory/urls.py
+++ inventory/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Inventory Management API is running </h1>")
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
