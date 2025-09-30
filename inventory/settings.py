f1c91925ae649aef38419c81d1ee1585b327f3c6
SSmikey <Seangnon09@gmail.com>
2025-09-30 12:27:52 +0700

    setup vercel

--- inventory/settings.py
+++ inventory/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from dotenv import load_dotenv
+load_dotenv()
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 import dj_database_url
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
